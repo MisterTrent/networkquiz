e7409c33f8b63c07ca0a47f7ac245295879935a5
MisterTrent <85028006+MisterTrent@users.noreply.github.com>
2022-05-18 20:51:27 -0400

    Updated homepage route
    With basic test. Homepage will be responsible for loading choices
    of quiz topics from db and displaying to user. Simple query so
    route now just needs a better template for displaying.

--- app/home.py
+++ app/home.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app.extensions import db
+from app.models import Topic
 
 home_bp = Blueprint('home', __name__)
 
+def get_topics():
+    #TODO only return those associated with 1+ questions?
+    results = db.session.query(Topic).all()
+    return [r.name for r in results]
+
 @home_bp.route('/')
 def index():
-   return 'hello world'
+    topics = get_topics()
+    return render_template('index.html', quiz_topics = topics)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,66 @@
 import pytest
 from app import create_app
+from app.models import Base, Topic
+from flask import template_rendered
+from app.extensions import db
 
 @pytest.fixture(scope='module')
 def app():
-    app = create_app(config_type='Test')
-    yield app
+    '''App instance with database for functional tests'''
+
+    application = create_app(config_type='Test')
+
+    with application.app_context():
+        Base.metadata.drop_all(db.engine)
+        Base.metadata.create_all(db.engine)
+
+    yield application
+    
+    with application.app_context():
+        db.session.remove()
+        Base.metadata.drop_all(db.engine)
 
 @pytest.fixture(scope='function')
 def client(app):
     return app.test_client()
 
+@pytest.fixture(scope='function')
+def captured_templates(app):
+    '''Taken verbatim from Flask's documentation on signals:
+    flask.palletsprojects.com/en/2.1.x/signals/#subscribing-to-signals
+    '''
+    recorded = []
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
 def test_config(app):
     '''Ensure proper configuration object used by app factory'''
-    
+     
     assert app.config.get('TESTING') is True
 
-def test_home(client):
+def test_home(app, client, captured_templates):
+    
+    tag = Topic(name='Topic1')
+
+    with app.app_context():
+        db.session.add(tag)
+        db.session.commit()
 
     response = client.get('/')
+    
     assert response.status_code == 200
+    assert len(captured_templates) == 1
+
+    template, context = captured_templates[0]
+
+    assert template.name == 'index.html'
+    assert 'Topic1' in context['quiz_topics']
+
+    with app.app_context():
+        db.session.query(Topic).delete()
+        db.session.commit()
