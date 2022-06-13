58025a560b6c98b145fcbe693df06ea053cd6e91
MisterTrent <85028006+MisterTrent@users.noreply.github.com>
2022-06-12 23:02:30 -0400

    Started building out quiz processing
    Utility functions for preparing database entries to display to
    user, plus processing user input from quiz form.  With tests.
    
    Not yet complete, just EOD committing.

--- app/quiz.py
+++ app/quiz.py
@@ -1,15 +1,92 @@
 from flask import Blueprint, session, render_template
 from sqlalchemy.orm import with_polymorphic
+import re
 from app.extensions import db
 from app.models import Topic, Question, MultipleChoice
 
-import pytest
-
 quiz_bp = Blueprint('quiz', __name__)
+
+def prep_multichoice(qlist):
+    '''Assigns a position for each correct answer in a list of multiple 
+    choice questions.
+    '''
+    
+    #keys for both are the question's index; index encoded in html attribute
+    #for the form inputs and parsed upon submission to match w/ answer key
+    answer_key = {}
+    choices = {}
+   
+    #random.shuffle's 'random' parameter deprecated, otherwise we could simply
+    #shuffle correct+incorrect AND rendering indices w/ the same seed param
+    for qnum, q in enumerate(qlist):
+
+        incorrect = q.incorrect.split(',')
+        correct = q.correct.split(',')
+        both = correct + incorrect
+        
+        n_correct = len(correct)
+        idxs = random.shuffle(list(range((n_incorrect + n_correct))))
+        
+        #front of list = correct choices indices; end of list = incorrects'
+        answer_key[qnum] = {'id' : q.id,
+                            'correct' : idxs[:n_correct],
+                            'incorrect' : idxs[n_correct:]}
+        
+        #list of question text matching order 
+        choices[qnum] = [None]* (n_incorrect+n_correct)
+        
+        for n,i in enumerate(idxs):
+            choices[qnum][i] = both[n]
+    
+    return answer_key, choices
+
+def score_input(user_answers, answer_key):
+    '''Compares input with the key created at time of quiz round's generation
+    '''
+    pass 
+
+def extract_answers(form):
+    '''Gets only the answer inputs from the user's quiz form which may have
+    other fields (e.g. csrf). Question input "name" html attribute expected
+    to have the form of "q##" where ## is the question's index in this round
+    as stored in session.
+    '''
+   
+    #very basic regex to discard unnecessary fields TODO detect user tampering
+    rgx = '(\d+)'    
+    is_question = lambda k : len(k) == 3 and k[0] == 'q' \
+                            and k[1].isdigit() and k[2] == ''
+    
+    questions = {}
+    
+    for k,v in form.items():
+        split_key = re.split(rgx, k)
+        if is_question(split_key):
+           questions[int(split_key[1])] = v 
+
+    return questions
 
 @quiz_bp.route('/submit', methods=['POST'])
 def question_submit():
-    pass
+    '''Processes a round of quiz questions to compare user input with
+    questions' answers.
+    '''
+
+    try:
+        form = request.form
+    except:
+        abort(400)
+    
+    idlist = session['active_questions']
+    polymap = with_polymorphic(Question, MultipleChoice)
+    questions = db.session.query(polymap)\
+                    .filter(Question.id.in_(q_ids))\
+                    .all()
+    
+    answers = extract_answers(form)
+    results = score_input(answers, questions) 
+    
+    return render_template('answerpage.html', results = results)
 
 @quiz_bp.route('/get', methods=['GET'])
 def get_questions():
@@ -31,7 +108,15 @@ def get_questions():
     questions = db.session.query(polymap)\
                     .filter(Question.id.in_(q_ids))\
                     .all()
+
+    answer_key, questions = prep_multichoice(questions)
+
+    session['active_questions'] = q_ids
+    session['question_ids'] = session['question_ids'][:-n or None]
+    session['answer_key'] = answer_key
     
     #TODO end quiz if not enough available? Indicate end in jinja context to display?
 
-    return render_template('quiz.html', questions = questions)
+    return render_template('quiz.html', 
+                                questions=questions, 
+                                answer_key=answer_key)
---
+++ tests/test_quiz.py
@@ -0,0 +1,108 @@
+import pytest
+from sqlalchemy.orm import with_polymorphic
+from werkzeug.datastructures import ImmutableMultiDict
+from app import create_app
+from app.models import Base, Topic, Question, MultipleChoice
+from app.extensions import db
+from app.quiz import prep_multichoice, extract_answers
+
+@pytest.fixture(scope='module')
+def app():
+    '''App instance with database for functional tests'''
+
+    application = create_app(config_type='Test')
+
+    with application.app_context():
+        Base.metadata.drop_all(db.engine)
+        Base.metadata.create_all(db.engine)
+
+    yield application
+    
+    with application.app_context():
+        db.session.remove()
+        Base.metadata.drop_all(db.engine)
+
+@pytest.fixture(scope='module')
+def db_questions(app):
+    '''Populates database with three topics and two questions.
+    Question 1 tagged w/ Topic 1, Question 2 w/ Topic 1 and 2. Topic 3 is
+    an 'orphan' with no associated question relationship.
+    '''
+    
+    #TODO scope='function' causes unique constraint error the second time
+    #this fixture runs; possibly due to sqlite's autoincrement issue
+    #docs.sqlalchemy.org/en/14/dialects/sqlite.html
+
+    t1 = Topic(name='Topic1')
+    t2 = Topic(name='Topic2')
+    t3 = Topic(name='Topic3')
+    
+    q1_data = {
+        'text' : 'test text',
+        'qtype' : 'multiple_choice',
+        'correct' : 'correct1',
+        'incorrect' : 'choice1, choice2, choice3'
+    }
+    
+    q2_data = {
+        'text' : 'test text',
+        'qtype' : 'multiple_choice',
+        'correct' : 'correct2',
+        'incorrect' : 'choice1, choice2, choice3'
+    }
+
+    q1 = MultipleChoice(**q1_data)
+    q2 = MultipleChoice(**q2_data)
+
+    q1.topics.append(t1)
+    q2.topics.extend((t1,t2))
+
+    with app.app_context():
+        db.session.add(q1)
+        db.session.add(q2)
+        db.session.add(t3)
+        db.session.commit()
+        
+    yield
+
+    with app.app_context():
+        db.session.query(Topic).delete()
+        db.session.query(Question).delete()
+        db.session.commit()
+
+def test_prep_multichoice(app):
+    
+    with app.app_context():
+        
+        polymap = with_polymorphic(Question, MultipleChoice)
+        questions = db.session.query(polymap).all()
+         
+        q_ids = [q.id for q in questions]
+
+        answer_key, choices = prep_multichoice(questions) 
+        
+        for i, (key,data) in enumerate(answer_key.items()):
+            assert q_ids[i] == data['id']
+
+            possible_answers = choices[key]
+
+            for idx in data['correct']:
+                assert possible_answers[idx] in questions[i].correct
+            
+            for idx in data['incorrect']:
+                assert possible_answers[idx] in questions[i].incorrect
+
+def test_extract_answers():
+
+    goodform = ImmutableMultiDict([('q1','0'), ('q2','3')])
+    goodout = extract_answers(goodform)
+    
+    assert len(goodout.keys()) == 2
+    assert goodout[1] == '0'
+    assert goodout[2] == '3'
+
+    badform = ImmutableMultiDict([('1','0'), ('q22a', '22'), ('q2','3')])
+    badout = extract_answers(badform)
+
+    assert len(badout.keys()) == 1
+    assert badout[2] == '3'
