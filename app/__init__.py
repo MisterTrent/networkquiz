0ccc0c0a700ed2668443da763dbed25072fed6e7
MisterTrent <85028006+MisterTrent@users.noreply.github.com>
2022-06-17 21:55:09 -0400

    Added flask-session extension
    Server side session needed for storing quiz info while ongoing.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import config
 from .home import home_bp #TODO import inside app_context?
 from .quiz import quiz_bp
-from app.extensions import db
+from app.extensions import db, sess
 
 def create_app(config_type = None):
     '''Application factory can take several possible configuration
@@ -16,6 +16,7 @@ def create_app(config_type = None):
     app.config.from_object(config_obj)
 
     db.init_app(app)
+    sess.init_app(app)
 
     app.register_blueprint(home.home_bp)
     app.register_blueprint(quiz_bp)
--- config.py
+++ config.py
@@ -10,8 +10,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_TYPE = os.environ.get('SESSION_TYPE', 'null')
 
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite://')
+
